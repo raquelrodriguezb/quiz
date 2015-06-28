185ed9aa46391c147225f0417db1b06afde80605
raquelrodriguezb <raquel.rodriguezb@gmail.com>
2015-06-28 22:38:39 +0200

    Índice temático

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -18,7 +18,13 @@ exports.index = function(req, res) {
   if (req.query.search!==undefined){
     busqueda="%"+ req.query.search.toLowerCase().replace(/\W+/g,"%")+ "%";
   }
-  models.Quiz.findAll({where:["lower(pregunta) like ?", busqueda],order:[['pregunta','ASC']]} ).then(
+  models.Quiz.findAll({
+      where:{ $or: [
+        ["lower(pregunta) like ?", busqueda],
+        ["lower(tema) like ?", busqueda]
+        ]}, 
+      order:[['pregunta','ASC']]} )
+  .then(
     function(quizes) {
     res.render('quizes/index.ejs', {quizes: quizes, errors: []});
   }
@@ -52,10 +58,11 @@ exports.edit = function(req, res) {
   res.render('quizes/edit', {quiz: quiz, errors: []});
 };
 
+
 // GET /quizes/new
 exports.new = function(req, res) {
   var quiz = models.Quiz.build(
-    {pregunta: "Pregunta", respuesta: "Respuesta"}
+    {pregunta: "Pregunta", respuesta: "Respuesta", tema:"Tema"}
   );
 
   res.render('quizes/new', {quiz: quiz, errors: []});
@@ -74,7 +81,7 @@ exports.create = function(req, res) {
         res.render('quizes/new', {quiz: quiz, errors: err.errors});
       } else {
         quiz // save: guarda en DB campos pregunta y respuesta de quiz
-        .save({fields: ["pregunta", "respuesta"]})
+        .save({fields: ["pregunta", "respuesta", "tema"]})
         .then( function(){ res.redirect('/quizes')}) 
       }      // res.redirect: Redirección HTTP a lista de preguntas
     }
@@ -85,6 +92,7 @@ exports.create = function(req, res) {
 exports.update = function(req, res) {
   req.quiz.pregunta  = req.body.quiz.pregunta;
   req.quiz.respuesta = req.body.quiz.respuesta;
+  req.quiz.tema = req.body.quiz.tema;
 
   req.quiz
   .validate()
@@ -94,7 +102,7 @@ exports.update = function(req, res) {
         res.render('quizes/edit', {quiz: req.quiz, errors: err.errors});
       } else {
         req.quiz     // save: guarda campos pregunta y respuesta en DB
-        .save( {fields: ["pregunta", "respuesta"]})
+        .save( {fields: ["pregunta", "respuesta", "tema"]})
         .then( function(){ res.redirect('/quizes');});
       }     // Redirección HTTP a lista de preguntas (URL relativo)
     }
